cef6fbb39c7a850d18c9d20b14ffe614731c25d6
Supermog <balint@gearedapp.co.uk>
2026-07-20 13:29:13 +0200

    chore: assign new function to button

--- apps/web/src/pages/HomePage.tsx
+++ apps/web/src/pages/HomePage.tsx
@@ -26,7 +26,7 @@ export default function HomePage() {
 
   return (
     <button
-      onClick={() => signOut()}
+      onClick={() => onSignOut()}
       disabled={isPending}
       className="w-full px-4 py-3 mt-6 font-bold text-white transition-colors bg-teal-800 rounded-md cursor-pointer hover:bg-teal-900 focus:outline-none focus:ring-2 focus:ring-teal-300 focus:ring-offset-2 focus:ring-offset-teal-600"
     >
