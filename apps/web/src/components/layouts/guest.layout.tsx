3fc4bb6f57b23ca1a14c9aa1addef7ef29c9d799
Microwave2000 <nagyzsadam@gmail.com>
2026-08-06 14:41:58 +0200

    chore: add background to guest layout

--- apps/web/src/components/layouts/guest.layout.tsx
+++ apps/web/src/components/layouts/guest.layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Header from "../common/Header";
 
 function GuestLayout() {
   return (
-    <div className="p-4">
+    <div className="bg-slate-400/80 min-h-screen">
+      <Header />
       <Outlet />
     </div>
   );
